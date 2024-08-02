d920d9cf28b81b2e1c0946bacfc2b57da2db0105
LoGiSTeRr <danikbor160@Gmail.com>
2024-08-02 08:46:12 +0400

    axios and apiclient are implemented

--- components/ToDoListManager.tsx
+++ components/ToDoListManager.tsx
@@ -3,12 +3,21 @@ import React, {useState} from 'react';
 import ToDoList from "@/components/ToDoList";
 import {v4} from "uuid";
 import TaskCreationForm from "@/components/TaskCreationForm";
+import apiClient from "@/libs/api";
+import {toast} from "react-hot-toast";
 
 const ToDoListManager = () => {
 
     const [tasks, setTasks] = useState<TaskType[]>([])
 
-    const handleAddTask = (newTask: TaskType) => {
+    const handleAddTask = async (newTask: TaskType) => {
+        try {
+            await apiClient.post("/task", newTask);
+            toast.success("Task submitted");
+        }
+        catch (e){
+
+        }
         setTasks([...tasks, {...newTask, id: v4() }]);
     };
 
@@ -20,13 +29,13 @@ const ToDoListManager = () => {
         const index = tasks.findIndex(task => task.id === id);
 
         if (index !== -1) {
-            // Create a new tasks array
+            // Create a new task array
             const newTasks = [...tasks];
 
             // Update the existing task with new data
             newTasks[index] = { ...newTasks[index], ...updatedTask };
 
-            // Update the tasks state
+            // Update the task state
             setTasks(newTasks);
         }
     }
